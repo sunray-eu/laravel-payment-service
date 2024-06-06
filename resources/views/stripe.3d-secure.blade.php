08117d99d1e6c6329acf633a475f4d5c3d53350b
Maroš Varchola <marosvarchola@sunray.sk>
2024-06-06 15:56:32 +0200

    feat(stripe): added view for stripe 3D secure

---
+++ resources/views/stripe.3d-secure.blade.php
@@ -0,0 +1,54 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Complete the security steps</div>
+
+                    <div class="card-body">
+                        <p>You need to follow some steps with your bank to complete this payment. Let's go!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    @push('scripts')
+    <script src="https://js.stripe.com/v3/"></script>
+    <script>
+        const stripe = Stripe('{{ config('services.stripe.key') }}');
+
+        stripe.handleCardAction("{{ $clientSecret }}")
+            .then(function (result) {
+                if (result.error) {
+                    window.location.replace("{{ route('cancelled') }}");
+                } else {
+                    window.location.replace("{{ route('update-transaction-status') }}");
+                }
+            });
+    </script>
+    @endpush
+</body>
+
+</html>
